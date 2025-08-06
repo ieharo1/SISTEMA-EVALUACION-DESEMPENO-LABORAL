daea04191281fb62116ecdeea59443cf2fd79015
ieharo1 <zackharo1@gmail.com>
2025-08-06 12:11:03 -0500

    Primer commit

---
+++ js/podo.js
@@ -0,0 +1,11 @@
+"use strict"
+ 
+const iconof = document.querySelector(".boton69");
+
+iconof.addEventListener("click",function()
+{
+    location = "https://www.patreon.com/podotutorials";
+})
+ 
+
+ 
